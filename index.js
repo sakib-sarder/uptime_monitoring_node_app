aaff77bf659d723056a9ba81eb01b8687b3a329a
sakib <shakibsorder964@gmail.com>
2023-07-26 08:53:33 +0600

    parsing request done

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 // Dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // app object - module scaffolding
 const app = {};
@@ -26,8 +28,28 @@ app.createServer = () => {
 
 // Handle Request and Response
 app.handleReqRes = (req, res) => {
-  // response handle
-  res.end("Hello programmer");
+  // request handle
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handle
+    res.end("Hello world!");
+  });
+
 };
 
 app.createServer();
